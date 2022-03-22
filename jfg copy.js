598cb8c92194d5845276f8c0273fc2fd00c4d2c7
joshraiborde <joshraiborde@gmail.com>
2022-03-22 17:48:17 -0400

    scratch paper

---
+++ jfg copy.js
@@ -0,0 +1,11 @@
+// <% posts.forEach(post => {%>
+ 
+  
+//       <%= post.content %>
+//       <a href="/posts/<%=post.title%>">Read More</a>
+//       <% }; %>
+// <%  }); %>
+
+// fruits.forEach(fruit => {
+//   fruit.name
+// });
