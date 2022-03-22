594a2f18334cde67568f357a6d788bba13e31bf3
joshraiborde <joshraiborde@gmail.com>
2022-03-22 18:25:26 -0400

    scratch

--- jfg copy.js
+++ jfg copy.js
@@ -9,3 +9,13 @@
 // fruits.forEach(fruit => {
 //   fruit.name
 // });
+
+Fruit.insertOne(pineapple, (error) => {
+  if (error) {
+    console.log(error + " " + now.toUTCString());
+  } else {
+    console.log(
+      "Successfully saved fruits to fruitsDB on " + now.toUTCString()
+    );
+  }
+});
